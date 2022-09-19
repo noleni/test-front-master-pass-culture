f70baedb6075cf1fd801b767b5f43babfc393751
Marine Ramillon <marine.ramillon@gmail.com>
2022-09-19 15:52:55 +0200

    exo 3 form

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import React, {useState} from "react";
 
 import NewsList from "./components/NewsList";
 import DropDown from "./components/UI/Dropdown";
+import AddNews from "./components/AddNews";
 
 function App() {
 
   const [selectedCategory, setSelectedCategory] = useState({ value: 'Tous', label: 'Tous' });
+  const [displayForm, setDisplayForm] = useState(false);
+  const [newItem, setNewItem] = useState([{}]);
 
   const selectionHandler = (data : any) => {
     if (selectedCategory.value !== 'Tous') {
@@ -17,12 +20,24 @@ function App() {
     }
   }
 
+  const displayFormhandler = (event : React.SyntheticEvent) => {
+    event.preventDefault();
+    setDisplayForm(true);
+  };
+
+  const addNewsHandler = (title:string, category:string, date:any, text:string) => {
+    setDisplayForm(false);
+    setNewItem([{ title: title, category: category, date: date, text: text }])
+  };
+
   return (
     <div className="App">
-      <DropDown defaultValue={selectedCategory}
+      {!displayForm &&<DropDown defaultValue={selectedCategory}
                 onChange={setSelectedCategory}
-      />
-      <NewsList onSelectCategory={selectionHandler}/>
+                /> }
+      {!displayForm && <button type="button" onClick={displayFormhandler}>Ajoute une news</button> }
+      {!displayForm && <NewsList onSelectCategory={selectionHandler} newItem={newItem} /> }
+      {displayForm && <AddNews onAddNews={addNewsHandler}  /> }
     </div>
   );
 }
